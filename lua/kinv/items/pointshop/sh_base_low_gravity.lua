4649a03e33b844b36e2befc41b753a35f3d27016
Valentin Funk <funk.valentin@gmail.com>
2017-06-05 17:27:26 +0200

    Step 4: Using a material icon

--- lua/kinv/items/pointshop/sh_base_low_gravity.lua
+++ lua/kinv/items/pointshop/sh_base_low_gravity.lua
@@ -1,5 +1,6 @@
 ITEM.baseClass	= "base_pointshop_item"
 ITEM.multiplier = 2
+ITEM.material = "pointshop2/winner2.png"
 
 function ITEM:PlayerSpawn( ply )
     if ply != self:GetOwner() then
@@ -12,3 +13,24 @@ function ITEM:PlayerSpawn( ply )
     end )
 end
 Pointshop2.AddItemHook( "PlayerSpawn", ITEM )
+
+-- Returns a Derma Control Name used to create the shop icon in normal mode
+function ITEM.static:GetPointshopIconControl( )
+	return "DPointshopMaterialIcon"
+end
+
+-- Returns a Derma Control Name used to create the shop icon in lowend mode
+function ITEM.static:GetPointshopLowendIconControl( )
+	return "DPointshopMaterialIcon"
+end
+
+-- Returns a derma icon that ised in the inventory
+function ITEM:getIcon( )
+	self.icon = vgui.Create( "DPointshopMaterialInvIcon" )
+	self.icon:SetItem( self )
+	return self.icon
+end
+
+function ITEM.static.GetPointshopIconDimensions( )
+	return Pointshop2.GenerateIconSize( 4, 4 )
+end
