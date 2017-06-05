683a965cbeb5fba037c2821715b057ca245d6aa0
Valentin Funk <funk.valentin@gmail.com>
2017-06-05 17:50:34 +0200

    Step 4: Putting it together - Adding the blueprint

--- lua/ps2/modules/lowgravity/sh_module.lua
+++ lua/ps2/modules/lowgravity/sh_module.lua
@@ -3,4 +3,14 @@ local MODULE = {}
 MODULE.Name = "PS2 Low Gravity"
 MODULE.Author = "Kamshak"
 
+MODULE.Blueprints = {
+	{
+		label = "Low Gravity",
+		base = "base_low_gravity",
+		icon = "pointshop2/crime1.png",
+		creator = "DLowGravityCreator",
+		tooltip = "Create low-gravity items."
+	}
+}
+
 Pointshop2.RegisterModule( MODULE )
